c66b137f1107c12e86a51d1186d9832dfe94c67a
Donald Cameron <donald.cameron@qa.com>
2026-09-17 16:22:17 +0100

    Updated file with exception handler

--- demo_regex_precompile.py
+++ demo_regex_precompile.py
@@ -6,16 +6,40 @@
 """
     DocString
 """
+import sys
 import re
 
-fh_in = open(r"f:\labs\words", mode="rt")
+def search_pattern(pattern=r"^.{19}$", file=r"f:\labs\words"):
+    try:
+        fh_in = open(file, mode="rt")
+    except FileNotFoundError as err:
+        print(f"Error: {err.args[0]}, {err.args[1]}, {err.filename}", file=sys.stderr)
+        sys.exit(1)
+    except PermissionError as err:
+        print(f"Error: {err.args[0]}, {err.args[1]}, {err.filename}", file=sys.stderr)
+        sys.exit(2)
+    except Exception as err:
+        print(f"Some other occurred - Investigate", file=sys.stderr)
+        sys.exit(3)
+    else:
+        # Executes if try block succeeds
+        reobj = re.compile(pattern) # Compiles pattern ONLY ONCE
+        for line in fh_in:
+            m = reobj.search(line)  # Match using PRECOMPILED Pattern
+            if m:
+                print(f"Matched {m.group()} on {line.rstrip()} at {m.start()}-{m.end()}")
+        fh_in.close() # Close file handle.
+    finally:
+        # ALWAYS executes!
+        print("And now for something completely different..")
 
-reobj = re.compile(r"^.{19}$") # Compiles pattern ONLY ONCE
+    return None
 
-for line in fh_in:
-    # m = re.search(r"^.{19}$", line)  # Match lines with exactly 19 chars
-    m = reobj.search(line)  # Match using PRECOMPILED Pattern
-    if m:
-        print(f"Matched {m.group()} on {line.rstrip()} at {m.start()}-{m.end()}")
+def main():
+    search_pattern()
+    return None
 
-fh_in.close() # Close file handle.
+# Namespace Trick
+if __name__ == "__main__":
+    main()
+    sys.exit(0)
